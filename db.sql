3a0504bda5df525a8f4d1cbe9f752be3e49dff5c
lituo <hslx111@gmail.com>
2013-03-06 18:55:20 +0800

    dbsql

---
+++ db.sql
@@ -0,0 +1,38 @@
+CREATE TABLE staff
+(
+	sid char(10) primary key, 
+	name char(20), 
+	age int, 
+	idnumber char(18), 
+	spic text, 
+	created timestamp default now()
+);
+
+CREATE TABLE record
+(
+	rid serial primary key, 
+	sid char(10), 
+	type char(10), 
+	rpic text, 
+	created timestamp 
+	default now(), 
+	foreign key (sid) references staff(sid)
+);
+
+CREATE TABLE admin
+(
+	aid serial primary key, 
+	name char(20), 
+	pwd char(32)
+);
+
+CREATE TABLE log
+(
+	lid serial primary key,
+	sid char(10),
+	type char(10),
+	lpic text,
+	created timestamp default now(),
+	static boolean default 0,
+	foreign key (sid) references staff(sid)
+);
